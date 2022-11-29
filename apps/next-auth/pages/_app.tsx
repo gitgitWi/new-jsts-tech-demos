65cf00531656dcd04c05f0de135b061a2c749269
gitgitWi <gitgitWi@gmail.com>
2022-11-29 13:54:53 +0000

    feat(NextAuth) client session provider 추가

--- apps/next-auth/pages/_app.tsx
+++ apps/next-auth/pages/_app.tsx
@@ -1,7 +1,14 @@
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+
 import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
 
 export default MyApp;
