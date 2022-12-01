fa307d8cc17ab64e069fadbe1bc56be2d3efae27
gitgitWi <57997672+gitgitWi@users.noreply.github.com>
2022-12-01 21:13:23 +0900

    build(zod) 의존성 설치; zod, tailwindcss

---
+++ apps/api-response-with-zod/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './pages/**/*.{tsx}',
+    './components/**/*.{tsx}',
+    './features/**/*.{tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = config;
