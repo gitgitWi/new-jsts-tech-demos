9b9119e68073cadf217b395c9bdda2630ac0f266
gitgitWi <57997672+gitgitWi@users.noreply.github.com>
2022-12-01 21:46:13 +0900

    fix(tailwind) 누락된 postcss config 파일 추가

--- apps/api-response-with-zod/pages/index.tsx
+++ apps/api-response-with-zod/pages/index.tsx
@@ -3,7 +3,7 @@ import { MainLayout } from '@features/layouts';
 export default function Home() {
   return (
     <MainLayout>
-      <main className="w-full h-full m-0 p-0">
+      <main className="w-full h-full m-0 p-0 bg-slate-600">
         <h1 className="w-full text-lg text-center">title</h1>
       </main>
     </MainLayout>
