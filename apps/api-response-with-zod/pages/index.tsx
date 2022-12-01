91e973339310b67998411715ca6855a118ae1159
gitgitWi <57997672+gitgitWi@users.noreply.github.com>
2022-12-01 21:52:55 +0900

    style(index-page) 배경/폰트 수정

--- apps/api-response-with-zod/pages/index.tsx
+++ apps/api-response-with-zod/pages/index.tsx
@@ -3,8 +3,10 @@ import { MainLayout } from '@features/layouts';
 export default function Home() {
   return (
     <MainLayout>
-      <main className="w-full h-full m-0 p-0 bg-slate-600">
-        <h1 className="w-full text-lg text-center">title</h1>
+      <main className="w-full h-full m-0 p-4 bg-neutral-800">
+        <h1 className="w-full text-3xl text-center text-neutral-100 font-mono">
+          title
+        </h1>
       </main>
     </MainLayout>
   );
