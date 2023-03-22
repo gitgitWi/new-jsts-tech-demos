b99ceed6284cecd34ed0977e3547c1a33068151c
gitgitWi <gitgitWi@gmail.com>
2023-03-22 06:37:20 +0000

    feat(with-zustand) use count store in page

--- apps/next-app-dir-with-zustand/src/app/page.tsx
+++ apps/next-app-dir-with-zustand/src/app/page.tsx
@@ -1,90 +1,47 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from './page.module.css';
+'use client';
 
-const inter = Inter({ subsets: ['latin'] });
+import { create } from 'zustand';
+
+type CountState = {
+  count: number;
+};
+type CountActions = {
+  plusCount: () => void;
+  minusCount: () => void;
+};
+
+const useCountStore = create<CountState & CountActions>((set) => ({
+  count: 0,
+  plusCount: () => set((preState) => ({ count: preState.count + 1 })),
+  minusCount: () => set((preState) => ({ count: preState.count - 1 })),
+}));
 
 export default function Home() {
+  const { count, plusCount, minusCount } = useCountStore();
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1 style={{ fontSize: '2rem', fontFamily: 'sans-serif' }}>Home Page</h1>
+      <hr style={{ width: '100%' }} />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          maxWidth: '600px',
+        }}
+      >
+        <h2>Count: {count}</h2>
+        <button onClick={plusCount}>Plus + 1 </button>
+        <button onClick={minusCount}>Minus - 1 </button>
       </div>
     </main>
   );
